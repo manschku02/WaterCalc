ef5995b0579d23ca6033d012754cedae142c06fc
manschkuluku <manschkuluku@gmail.com>
2024-10-01 07:38:37 +0000

    Grundgerüst

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,37 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:myapp/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  runApp(MyApp());
+}
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Water Consumption Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: WaterCalculator(),
+    );
+  }
+}
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+class WaterCalculator extends StatefulWidget {
+  @override
+  _WaterCalculatorState createState() => _WaterCalculatorState();
+}
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+class _WaterCalculatorState extends State<WaterCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Water Consumption Calculator'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Water Calculator!'),
+      ),
+    );
+  }
 }
