d5a4100be05f73db350e2a4b7aca89293b4d870b
manschkuluku <manschkuluku@gmail.com>
2024-10-01 07:58:13 +0000

    Berechnung der Lichtjahre hinzufügen.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -24,6 +24,17 @@ class WaterCalculator extends StatefulWidget {
 
 class _WaterCalculatorState extends State<WaterCalculator> {
   final TextEditingController _controller = TextEditingController();
+  String _result = '';
+
+  void _calculateLightYears() {
+    double ml = double.tryParse(_controller.text) ?? 0;
+    double waterMoleculeLength =
+        ml * 1e-6; // Beispielwert für die Länge der Wasserstoffmoleküle in km
+    double lightYears = waterMoleculeLength / 9.461e12; // Lichtjahr in km
+    setState(() {
+      _result = 'Molecules lined up: ${lightYears.toStringAsFixed(2)} Lj';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +53,11 @@ class _WaterCalculatorState extends State<WaterCalculator> {
               ),
               keyboardType: TextInputType.number,
             ),
+            ElevatedButton(
+              onPressed: _calculateLightYears,
+              child: Text('Calculate'),
+            ),
+            Text(_result),
           ],
         ),
       ),
